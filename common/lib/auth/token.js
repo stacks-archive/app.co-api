9bd3cb99fc025bb24b933ecd78336d8e780878af
Hank Stoever <hstove@gmail.com>
2018-05-24 14:18:36 -0700

    save and return JWT in auth flow

---
+++ common/lib/auth/token.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+  createToken: (user) => {
+    const data = {
+      id: user.id,
+      username: user.blockstackUsername,
+    };
+
+    const tokenOptions = {
+      algorithm: 'HS256',
+    };
+
+    return jwt.sign({ data }, process.env.JWT_SECRET, tokenOptions);
+  },
+};
--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Mailchimp = require('mailchimp-api-v3');
 const crypto = require('crypto');
+require('isomorphic-fetch');
+// const blockstack = require('blockstack');
+const { verifyAuthResponse } = require('blockstack/lib/auth/authVerification');
+const { decodeToken } = require('jsontokens');
 
 require('dotenv').config();
 
 const mailchimp = new Mailchimp(process.env.MAILCHIMP_KEY);
 
-const { App } = require('./db/models');
+const { App, User } = require('./db/models');
 const { saveRanking } = require('./common/lib/twitter');
 const appConstants = require('./db/models/constants/app-constants');
 const GSheets = require('./common/lib/gsheets');
+const { createToken } = require('./common/lib/auth/token');
 
 const port = parseInt(process.env.PORT, 10) || 4000;
 
@@ -82,6 +87,35 @@ app.post('/api/subscribe', async (req, res) => {
   });
 
   res.json({ success: true });
+});
+
+app.post('/api/authenticate', async (req, res) => {
+  const { authToken } = req.query;
+  if (!authToken) {
+    return res.status(400).json({ success: false });
+  }
+
+  const nameLookupURL = 'https://core.blockstack.org/v1/names/';
+  if (!(await verifyAuthResponse(authToken, nameLookupURL))) {
+    console.log('Invalid auth response');
+    return res.status(400).json({ success: false });
+  }
+
+  const { payload } = decodeToken(authToken);
+  console.log(payload);
+
+  const userAttrs = {
+    blockstackUsername: payload.username,
+  };
+
+  const [user] = await User.findOrBuild({ where: userAttrs, defaults: userAttrs });
+  await user.update({
+    blockstackDID: payload.iss,
+  });
+  console.log(user.id);
+  const jwt = createToken(user);
+
+  return res.json({ success: true, token: jwt });
 });
 
 app.listen(port, (err) => {
