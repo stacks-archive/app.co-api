0ad3c78a9b5e18dc379cc2f21853d450d503db3c
Hank Stoever <hstove@gmail.com>
2018-05-24 14:18:27 -0700

    User.blockstackUsername

--- db/migrations/20180524202614-create-user.js
+++ db/migrations/20180524202614-create-user.js
@@ -1,33 +1,32 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       blockstackDID: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+      },
+      blockstackUsername: {
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
+        type: Sequelize.DATE,
+      },
+    }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Users'),
 };
--- db/models/user.js
+++ db/models/user.js
@@ -1,10 +1,14 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    blockstackDID: DataTypes.STRING,
-    name: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      blockstackDID: DataTypes.STRING,
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      blockstackUsername: DataTypes.STRING,
+    },
+    {},
+  );
   User.associate = function(models) {
     // associations can be defined here
   };
