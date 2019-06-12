c7a10c40b9ada8fbad2fbb97b7fea57ace6a007f
Hank Stoever <hstove@gmail.com>
2019-06-12 13:34:55 -0700

    adds migration for stx info

--- db/migrations/20190612194705-add_stx_info.js
+++ db/migrations/20190612194705-add_stx_info.js
@@ -1,10 +1,10 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     queryInterface.addColumn('Apps', 'stacksAddress', Sequelize.STRING);
-    queryInterface.addColumn('Apps', 'hasCollectedKYC', Sequelize.STRING);
-    queryInterface.addColumn('Apps', 'hasAcceptedSECTerms', Sequelize.STRING);
-    queryInterface.addColumn('Apps', 'hasAcceptedTerms', Sequelize.STRING);
-    queryInterface.addIndex('Apps', { fields: ['status'] });
+    queryInterface.addColumn('Apps', 'hasCollectedKYC', Sequelize.BOOLEAN);
+    queryInterface.addColumn('Apps', 'hasAcceptedSECTerms', Sequelize.BOOLEAN);
+    queryInterface.addColumn('Apps', 'hasAcceptedTerms', Sequelize.BOOLEAN);
+    queryInterface.addIndex('Apps', { fields: ['status'], name: 'Appsstatus' });
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -15,6 +15,11 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
+    queryInterface.removeColumn('Apps', 'stacksAddress', Sequelize.STRING);
+    queryInterface.removeColumn('Apps', 'hasCollectedKYC', Sequelize.BOOLEAN);
+    queryInterface.removeColumn('Apps', 'hasAcceptedSECTerms', Sequelize.BOOLEAN);
+    queryInterface.removeColumn('Apps', 'hasAcceptedTerms', Sequelize.BOOLEAN);
+    queryInterface.removeIndex('Apps', 'Appsstatus');
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
