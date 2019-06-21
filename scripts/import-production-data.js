23647d1c9a8c60ac0aeddcc988bede24533be2ba
Hank Stoever <hstove@gmail.com>
2019-06-21 11:19:02 -0700

    include payouts in import-from-prod script

--- scripts/import-production-data.js
+++ scripts/import-production-data.js
@@ -4,7 +4,14 @@ const _ = require('lodash');
 const Promise = require('bluebird');
 require('dotenv').config();
 
-const { App, Ranking, MiningMonthlyReport, MiningReviewerReport, MiningReviewerRanking } = require('../db/models');
+const {
+  App,
+  Ranking,
+  MiningMonthlyReport,
+  MiningReviewerReport,
+  MiningReviewerRanking,
+  MiningAppPayout,
+} = require('../db/models');
 const { clearCache } = require('../common/lib/utils');
 
 const apiURL = 'https://api.app.co';
@@ -60,6 +67,9 @@ const fetchAppMiningData = async () => {
       const rankings = await MiningReviewerRanking.findAll({ where: { reportId: existing.id } });
       const deletes = rankings.map((r) => r.destroy());
       await Promise.all(deletes);
+      const payouts = await MiningAppPayout.findAll({ where: { reportId: existing.id } });
+      const payoutDeletes = payouts.map((p) => p.destroy());
+      await Promise.all(payoutDeletes);
       await existing.destroy();
     }
     const newReport = new MiningMonthlyReport({
@@ -73,7 +83,7 @@ const fetchAppMiningData = async () => {
     });
     await newReport.save();
 
-    Promise.map(report.MiningReviewerReports, async (reviewerReport) => {
+    await Promise.map(report.MiningReviewerReports, async (reviewerReport) => {
       const newReviewerReport = new MiningReviewerReport({
         reportId: newReport.id,
         reviewerName: reviewerReport.reviewerName,
@@ -93,6 +103,19 @@ const fetchAppMiningData = async () => {
         await newRanking.save();
         return newRanking;
       });
+    });
+
+    await Promise.map(report.MiningAppPayouts, async (payout) => {
+      const app = await App.findOne({ where: { productionId: payout.appId } });
+      if (!app) {
+        return true;
+      }
+      const newPayout = new MiningAppPayout({
+        BTCPaymentValue: payout.BTCPaymentValue,
+        appId: app.id,
+        reportId: newReport.id,
+      });
+      return newPayout.save();
     });
     await newReport.update({
       status: 'published',
